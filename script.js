e4b0e4492f726ceffbdc418407b5a4f0af84d41d
Angel Chavez <>
2022-08-01 21:05:54 +0000

    Day 1

--- script.js
+++ script.js
@@ -4,38 +4,51 @@ const submit = document.querySelector("#button")
 const inputField = document.querySelector("#guess")
 const numText = document.querySelector("#num")
 const lowHi = document.querySelector("#lowHi")
+const pastGuesses = document.querySelector("#attempt")
 let guesses = 0
 let previousGuesses = []
 
-submit.addEventListener("click", function(e){
+submit.addEventListener("click", function(e) {
   const guess = parseInt(inputField.value);
   nancheck(guess)
 })
 
 function nancheck(guess) {
-  if (isNaN(guess)){
+  if (isNaN(guess)) {
     alert("That is not a valid guess");
   } else if (guess < 1) {
     alert("Enter a number greater than 1")
   } else if (guess > 100) {
     alert("Enter a number less than 100")
   } else {
+
     previousGuesses.push(guess);
     checkGuess(guess)
-  } 
+    showGuess(guess)
+    if (guesses === 9) {
+      endMessage(`You got it! The number was ${randomNumber}`)
+    }
+  }
 }
 
-function checkGuess(guess){
-  if (guess ===randomNumber){
+function showGuess(guess) {
+  guesses++;
+  console.log(guesses);
+  numText.innerHTML = `<h1>Attempt remaining : ${10 - guesses}</h1>`;
+  inputField.value = ""
+  pastGuesses.innerHTML += `${guess} `
+}
+
+function checkGuess(guess) {
+  if (guess === randomNumber) {
     endMessage("Correct! You Win")
-  } else if (guess < randomNumber){
+  } else if (guess < randomNumber) {
     endMessage("Too low! Try again")
-  }else if (guess > randomNumber){
+  } else if (guess > randomNumber) {
     endMessage("Too high! Try again")
   }
 }
 
-function endMessage(message){
+function endMessage(message) {
+  lowHi.innerHTML = `<h1>${message}</h1>`
 }
-// // For the past guesses part(?), but this one is for images we might need to change it to numbers later
-// imageHolderDiv.innerHTML = `<img src="${imgurl}"/>` + imageHolderDiv.innerHTML;
