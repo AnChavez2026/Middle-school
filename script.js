f9f53b16d8ae80cebcadf8dcacf2244c9f0366ce
Angel Chavez <>
2022-08-02 18:17:27 +0000

    day 2

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-let randomNumber = Math.floor(Math.random() * 101);
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+let randomNumber = getRandomInt(100)
 console.log(randomNumber);
 const submit = document.querySelector("#button")
 const inputField = document.querySelector("#guess")
 const numText = document.querySelector("#num")
 const lowHi = document.querySelector("#lowHi")
 const pastGuesses = document.querySelector("#attempt")
-let guesses = 0
-let previousGuesses = []
+const newGameButton = document.querySelector("#button2");
+const unhide = document.querySelector("#unhide");
+const imageHolderDiv = document.querySelector("#imageholder");
+let guesses = 0;
+let winURL = "https://api.giphy.com/v1/gifs/search?api_key=N1Q0iOh5AufGTWWzFtw1awPYg21xiZRp&q=winner&limit=25&offset=0&rating=g&lang=en";
+let lURL = "https://api.giphy.com/v1/gifs/search?api_key=N1Q0iOh5AufGTWWzFtw1awPYg21xiZRp&q=loser&limit=25&offset=0&rating=g&lang=en";
+let win = false;
 
 submit.addEventListener("click", function(e) {
   const guess = parseInt(inputField.value);
-  nancheck(guess)
+  nancheck(guess);
+  winCheck()
 })
 
 function nancheck(guess) {
@@ -21,12 +30,12 @@ function nancheck(guess) {
   } else if (guess > 100) {
     alert("Enter a number less than 100")
   } else {
-
-    previousGuesses.push(guess);
     checkGuess(guess)
     showGuess(guess)
-    if (guesses === 9) {
-      endMessage(`You got it! The number was ${randomNumber}`)
+    if (guesses === 10) {
+      endMessage(`Game Over! The number was ${randomNumber}`)
+      checkGuess(guess)
+      gameOver()
     }
   }
 }
@@ -42,6 +51,8 @@ function showGuess(guess) {
 function checkGuess(guess) {
   if (guess === randomNumber) {
     endMessage("Correct! You Win")
+    gameOver()
+    win = true;
   } else if (guess < randomNumber) {
     endMessage("Too low! Try again")
   } else if (guess > randomNumber) {
@@ -53,8 +64,38 @@ function endMessage(message) {
   lowHi.innerHTML = `<h1>${message}</h1>`;
 }
 
-function gameOver(){
+function gameOver() {
   inputField.setAttribute("disabled", "");
   button.setAttribute("disabled", "");
-  
+  //newGameButton.innerHTML
+  unhide.classList.remove("section")
+  unhide.classList.remove("hidden")
+  //.innerHTML = 'Press "start over" to restart the game!'
+  newGameButton.addEventListener("click", function() {
+    inputField.removeAttribute("disabled")
+    button.removeAttribute("disabled")
+    lowHi.innerHTML = ""
+    guesses = 0
+    pastGuesses.innerHTML = ""
+    numText.innerHTML = `<h1>Attempt remaining : 10</h1>`;
+  })
+}
+
+async function winCheck() {
+  if (win == false && guesses == 10) {
+    const response = await fetch(lURL);
+    const json = await response.json();
+    let i = getRandomInt(25);
+    const gifURL = json.data[i].images.downsized.url;
+    console.log(gifURL);
+    imageHolderDiv.innerHTML = `<img src = ${gifURL}/>`
+  }
+  else if (win == true) {
+    const response = await fetch(winURL);
+    const json = await response.json();
+    let i = getRandomInt(25);
+    const gifURL = json.data[i].images.downsized.url;
+    console.log(gifURL);
+    imageHolderDiv.innerHTML = `<img src = ${gifURL}/>`;
+  }
 }
